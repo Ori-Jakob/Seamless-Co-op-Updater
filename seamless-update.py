c948cdcabbc9d3e8d8ce925d71c4cb197ef6d38c
Ori Jakob <ori.jakob@gmail.com>
2025-02-24 15:16:08 -0500

    feat: add support for DS3 Seamless
    fix: os.path for pyinstaller now points to the location of the exe itself
    
    feat: dynamically detects game

--- seamless-update.py
+++ seamless-update.py
@@ -1,4 +1,4 @@
-import subprocess, os, time, configparser
+import subprocess, os, sys, time, configparser
 
 
 
@@ -8,7 +8,6 @@ def check_dependency(package):
     try:
       return __import__(package)
     except ImportError:
-      import sys
       subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
 
@@ -52,37 +51,60 @@ def checkForUpdate(data):
 
 
 
-script_dir = os.path.dirname(os.path.realpath(__file__)) 
+script_dir = str()
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    script_dir = os.path.dirname(sys.executable)
+else:
+    script_dir = os.path.dirname(__file__)
+
 _sleep_duration = 5
+game = dict()
 
-
+def set_game():
+  global game
+  if script_dir.upper().count("ELDEN RING") > 0:
+    game = {
+      "url": "https://github.com/LukeYui/EldenRingSeamlessCoopRelease/releases/latest",
+      "code": "er",
+      "name": "ELDEN RING",
+      "exe": "eldenring.exe"
+    }
+  else:
+    game = {
+      "url": "https://github.com/LukeYui/DarkSouls3SeamlessCoopRelease/releases/latest",
+      "code": "ds3",
+      "name": "DARK SOULS III",
+      "exe": "DarkSoulsIII.exe"
+    }
 
 
 
 def main():
 
-  global _sleep_duration
-
+  global _sleep_duration, game
+  print(script_dir)
+  print(f"Detected Game: {game["name"]}\n")
   #Grab the original configuration
   original_config, new_config = configparser.ConfigParser(), configparser.ConfigParser()
 
 
   #check if original ini exists for config transfer
-  if os.path.exists('./SeamlessCoop/ersc_settings.ini'):
-    original_config.read('./SeamlessCoop/ersc_settings.ini') 
+  if os.path.exists(f'./SeamlessCoop/{game["code"]}sc_settings.ini'):
+    original_config.read(f'./SeamlessCoop/{game["code"]}sc_settings.ini') 
   else:
     original_config = None
 
 
   #Check if update exists based on previous install
 
-  data = requests.get("https://github.com/LukeYui/EldenRingSeamlessCoopRelease/releases/latest").url[70:]
+  data = requests.get(game["url"]).url[70:]
   if not checkForUpdate(data): return #don't bother running everything else if already on the latest version
   
     
   
   #Install the new update if needed
-  url = 'https://github.com/LukeYui/EldenRingSeamlessCoopRelease/releases/latest/download/ersc.zip'
+  url = f'{game["url"]}/download/ersc.zip'
 
   print(f'Downloading Seamless Coop release {data}...')
   dload.save_unzip(url, './', delete_after=True) #unzip to root of script file
@@ -96,7 +118,7 @@ def main():
 #Apply original configuration to the new configuration if old one exists
   if original_config != None:
 
-    new_config.read('./SeamlessCoop/ersc_settings.ini')
+    new_config.read(f'./SeamlessCoop/{game["code"]}sc_settings.ini')
     print('Transferring configuration file...')
 
     for x in new_config:
@@ -104,7 +126,7 @@ def main():
           if i in original_config[x]: #check to see if parameter exists in the original. We don't want an error for accessing an invalid parameter.
             new_config[x][i] = original_config[x][i] 
 
-    with open('./SeamlessCoop/ersc_settings.ini', 'w') as config:
+    with open(f'./SeamlessCoop/{game["code"]}sc_settings.ini', 'w') as config:
         new_config.write(config) #due to limitations of python lib configparser, comments will not be transfered to the new config
 
     print("Done!\n")
@@ -119,10 +141,10 @@ def main():
 
 
 if __name__ == "__main__":
-   
-  path = "ELDEN RING\\Game" if os.name == "nt" else "ELDEN RING/Game"
+  set_game()
+  path = f"{game["name"]}\\Game" if os.name == "nt" else f"{game["name"]}/Game"
   
-  if os.path.exists('./eldenring.exe'):
+  if os.path.exists(f'./{game["exe"]}'):
     main()
   else:
     print(f"ERROR: script is in the wrong location.'")
@@ -130,5 +152,3 @@ if __name__ == "__main__":
 
   print(f'Exiting in {_sleep_duration} seconds...')
   time.sleep(_sleep_duration)
-
-
