1456d2bf466a9ba4822b572d88b6c13e1f5f2b8c
Ori Jakob <ori.jakob@gmail.com>
2025-02-24 18:15:27 -0500

    feat: support launching DS3 with batch file

--- start-with-update.bat
+++ start-with-update.bat
@@ -8,13 +8,26 @@ IF EXIST .\seamless-update.exe (
 	ECHO 'seamless-update' not found...
 )
 
-
 ECHO Launching game...
 
-IF EXIST .\modengine2_launcher.exe (
-    .\modengine2_launcher.exe -t er -c .\config_eldenring.toml
+IF EXIST .\DarkSoulsIII.exe (
+    SET config_name=config_darksouls3.toml
+    SET launcher=ds3sc_launcher.exe
+) ELSE IF EXIST .\eldenring.exe (
+    SET config_name=config_eldenring.toml
+    SET launcher=ersc_launcher.exe
+)
+IF NOT "%luancher%"=="" (
+    IF EXIST .\modengine2_launcher.exe (
+        ECHO Launching ModEngine2 with config file: '%config_name%'
+        .\modengine2_launcher.exe -t er -c .\%config_name%
+    ) ELSE IF EXIST .\%launcher% (
+        ECHO Launching '%launcher%'
+        .\%launcher%
+    )
 ) ELSE (
-    .\ersc_launcher.exe
+    ECHO Could not find 'modengine2_launcher.exe' or 'seamless co-op launcher'
+    ECHO Are we in the right directory?
 )
 
 timeout /t 2 >NUL
