f51d995a9fe3aa59161a618b1047011b5a0c3db0
Ori Jakob <ori.jakob@gmail.com>
2025-03-09 22:36:23 -0400

    made pathing more dynamic

--- seamless-update.py
+++ seamless-update.py
@@ -6,6 +6,7 @@ class Game:
       self.url = str()
       self.code = str()
       self.exe = str()
+      self.mod_folder = "SeamlessCoop"
       self.found = False
 
 
@@ -42,13 +43,13 @@ def checkForUpdate(data):
 
   global _sleep_duration
 
-  if not os.path.exists("./SeamlessCoop"):
+  if not os.path.exists(f"./{game.mod_folder}"):
       print(f"Seamless Coop not found. ")
       return True
 
       
   try:
-    with open('./SeamlessCoop/version.txt', 'r') as version:
+    with open(f'./{game.mod_folder}/version.txt', 'r') as version:
       ver = version.readline()
       if ver == data:
         print(f"Already Updated to the latest version ({data}). Skipping Update...")
@@ -56,7 +57,7 @@ def checkForUpdate(data):
         return False
 
   except OSError:
-    with open('./SeamlessCoop/version.txt', 'w') as version:
+    with open(f'./{game.mod_folder}/version.txt', 'w') as version:
       version.write(data)
 
   print(f"New update found! -> {data}")
@@ -66,20 +67,20 @@ def checkForUpdate(data):
 
 def set_game():
   global game
-  if script_dir.upper().count("ELDEN RING") > 0:
+  if script_dir.upper().count(os.path.join("ELDEN RING", "Game")) > 0:
     game.url = "https://github.com/LukeYui/EldenRingSeamlessCoopRelease/releases/latest"
     game.code = "er"
     game.name = "ELDEN RING"
     game.exe = "eldenring.exe"
     game.found = True
-  elif script_dir.upper().count("DARK SOULS III") > 0:
+  elif script_dir.upper().count(os.path.join("ELDEN RING", "Game")) > 0:
     game.url = "https://github.com/LukeYui/DarkSouls3SeamlessCoopRelease/releases/latest"
     game.code = "ds3"
     game.name = "DARK SOULS III"
     game.exe = "DarkSoulsIII.exe"
     game.found = True
   else:
-    game.name = ' or '.join(["'{0}{1}Game'".format(x,"\\" if os.name == "nt" else "/") for x in SUPPORTED_GAMES])
+    game.name = " or ".join(["'" + os.path.join(f"{x}", "Game") + "'" for x in SUPPORTED_GAMES])
 
 
 
@@ -94,8 +95,8 @@ def main():
 
 
   #check if original ini exists for config transfer
-  if os.path.exists(f'./SeamlessCoop/{game.code}sc_settings.ini'):
-    original_config.read(f'./SeamlessCoop/{game.code}sc_settings.ini') 
+  if os.path.exists(f'./{game.mod_folder}/{game.code}sc_settings.ini'):
+    original_config.read(f'./{game.mod_folder}/{game.code}sc_settings.ini') 
   else:
     original_config = None
 
@@ -125,14 +126,14 @@ def main():
   print('Done!\n')
 
 #update version.txt
-  with open('./SeamlessCoop/version.txt', 'w') as version:
+  with open(f'./{game.mod_folder}/version.txt', 'w') as version:
       version.write(data)
 
 
 #Apply original configuration to the new configuration if old one exists
   if original_config != None:
 
-    new_config.read(f'./SeamlessCoop/{game.code}sc_settings.ini')
+    new_config.read(f'./{game.mod_folder}/{game.code}sc_settings.ini')
     print('Transferring configuration file...')
 
     for x in new_config:
@@ -140,7 +141,7 @@ def main():
           if i in original_config[x]: #check to see if parameter exists in the original. We don't want an error for accessing an invalid parameter.
             new_config[x][i] = original_config[x][i] 
 
-    with open(f'./SeamlessCoop/{game.code}sc_settings.ini', 'w') as config:
+    with open(f'./{game.mod_folder}/{game.code}sc_settings.ini', 'w') as config:
         new_config.write(config) #due to limitations of python lib configparser, comments will not be transfered to the new config
 
     print("Done!\n")
@@ -160,7 +161,7 @@ if __name__ == "__main__":
   if game.found and os.path.exists(f'./{game.exe}'):
     main()
   else:
-    print(f"ERROR: script is in the wrong location.'")
+    print(f"ERROR: script is in the wrong location.")
     print(f"The file is in the wrong path. Make sure it is placed and ran from {game.name}.")
 
   for x in range(_sleep_duration):
